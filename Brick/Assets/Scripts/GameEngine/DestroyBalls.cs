c685b81e785a85b1ba036be3cb6d094555f91bd5
MobileWebLinja <67433114+MobileWebLinja@users.noreply.github.com>
2020-10-15 18:40:59 -0700

    version 1.0

---
+++ Brick/Assets/Scripts/GameEngine/DestroyBalls.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class DestroyBalls : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "ball" && LevelManager.instance.gatherBalls == false)
+        {
+            Balls.instance.nArrivedBallsBack++;
+            Destroy(collision.gameObject);
+        }
+    }
+}
